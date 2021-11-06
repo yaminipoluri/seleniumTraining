8b51eac83dab23eb296b1ac41fd7becd727ab4bc
yaminipoluri <yamini.poluri@gmail.com>
2021-11-06 15:06:40 +0800

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+package com.bookssupply.primaryschool;
+
+public class HelloWorld
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world");
+		System.out.println("welcome");// TODO Auto-generated method stub
+        test t1= new test();
+        t1.print();
+        
+	}
+}
+
+class test
+{
+	void print()
+	{
+		System.out.println("yamini - welcome project");
+	}
+}
